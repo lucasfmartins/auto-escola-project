4a10449cea681ea5be1d3d78732b313072c748da
lucas.martins <lucas.martins@bit.com.br>
2021-04-06 16:31:13 -0300

    Notfound page

--- src/routes/servicos/index.js
+++ src/routes/servicos/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+
+import Error from "components/pages/Error";
 import ProductDetailPage from "components/pages/ProductDetail";
+import NotFoundImage from "draws/Error";
+
 import { useScrollToTop } from "hooks/scroll";
 import { useProduct } from "hooks/products";
 
@@ -10,6 +14,16 @@ const ProductDetail = () => {
 
   const { slang } = useParams();
   const product = useProduct({ slang });
+
+  if (!product) {
+    return (
+      <Error
+      image={<NotFoundImage />}
+        title="Serviço não encontrado"
+        description="Serviço não encontrado ou não disponível"
+      />
+    );
+  }
 
   return <ProductDetailPage product={product} />;
 };
